3a44923233d9cad965acd09596f6126713fd0547
Siwakorn Petchuchuay <ibeerz.s@gmail.com>
2016-04-21 16:28:17 +0700

    Add menu for LNG course work

--- js/custom.js
+++ js/custom.js
@@ -17,22 +17,34 @@ $(function(){
 	}
 	setTimeout(function(){imrandom();}, 1000);
 	$('.iam').on('mouseenter', function(){
-		$(this).find('p').fadeIn(200);
 		$('.iambg').fadeIn(200);
 	});
 
 	$('.iam').on('mouseleave', function(){
-		$(this).find('p').fadeOut(200);
 		$('.iambg').fadeOut(200);
 	});
 
 	$('.timeline').on('mouseenter', function(){
-		$(this).find('p').fadeIn(200);
 		$('.timelinebg').fadeIn(200);
 	});
 
 	$('.timeline').on('mouseleave', function(){
-		$(this).find('p').fadeOut(200);
 		$('.timelinebg').fadeOut(200);
+	});
+
+	$('.btn-circle').on('mouseenter', function(){
+		$(this).find('p').fadeIn(200);
+	});
+
+	$('.btn-circle').on('mouseleave', function(){
+		$(this).find('p').fadeOut(200);
+	});
+
+	$('[data-box]').on('click', function(){
+		$('#'+$(this).data('box')).fadeIn(200);
+	});
+
+	$('.box .close').on('click', function(){
+		$('.box').fadeOut(200);
 	});
 });
